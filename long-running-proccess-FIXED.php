5bed35f5f1139d9ce433ec4a3453544c0b37cc9b
Marcial Paul Gargoles <im.codename@gmail.com>
2020-02-10 06:45:09 +0800

    end output buffering

--- long-running-proccess-FIXED.php
+++ long-running-proccess-FIXED.php
@@ -7,7 +7,9 @@
 // sending a data.
 $spacer_size = 8; // increment me until it works
 echo str_pad('', (1024 * $spacer_size), "\n"); // send 8kb of new line to browser (default), just make sure that this new line will not affect your code.
-// if you have output compression, make sure your data will reach >8KB. 
+// if you have output compression, make sure your data will reach >8KB.
+
+if(ob_get_level()) ob_end_clean(); // end output buffering
 
 sleep(110);
 echo "WOW, you waited me for 110 seconds.";
